e07953251f0a185cab2fe2c3ec38aa7a38438152
Marko Burjek <s0mebody.slo@gmail.com>
2012-10-19 11:37:06 +0200

    Written Harmony search
    TODO: add parametes. Change some parameters to class parameters. Maybe
    different datastructures.

--- HarmonySearch/src/si/mabu/harmony/HarmonySearch.java
+++ HarmonySearch/src/si/mabu/harmony/HarmonySearch.java
@@ -3,6 +3,7 @@ package si.mabu.harmony;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import org.um.feri.ears.algorithms.Algorithm;
 import org.um.feri.ears.algorithms.AlgorithmInfo;
@@ -21,6 +22,7 @@ public class HarmonySearch extends Algorithm {
 	double range = 0.05;
 	int max_iter = 0;
 	int factor = 1;
+	Random random;
 	
 	public HarmonySearch() {
 		this.debug = false;
@@ -31,12 +33,14 @@ public class HarmonySearch extends Algorithm {
 	public HarmonySearch(boolean d) {
 		super();
 		setDebug(d);
+		random = new Random();
 	}
 	
 	/**
+	 * Initialize memory with mem_size * factor. Orders memory by fitness decreasing.
 	 * @param taskProblem
 	 * @return
-	 * Initialize memory with mem_size * factor. Then returns first mem_size individuals.
+	 *  first mem_size individuals.
 	 * @throws StopCriteriaException
 	 */
 	private List<Individual> initialize_harmony_memory(Task taskProblem) throws StopCriteriaException {
@@ -48,17 +52,71 @@ public class HarmonySearch extends Algorithm {
 		return memory.subList(0, mem_size);
 	}
 	
+	/**
+	 * 
+	 * @param min value
+	 * @param max value
+	 * @return
+	 * random number between min <= x max
+	 */
+	private double random_double_in_constraints(double min, double max) {
+		return min + (random.nextDouble() * (max - min));
+	}
+	
+	/**
+	 * Changes values of parameters
+	 * @param memory
+	 * @param taskProblem
+	 * @return
+	 */
+	private double[] create_harmony(List<Individual> memory, Task taskProblem) {
+		double[] ds = new double[this.factor];
+		for (int i = 0; i < this.factor; i++) {
+			if (random.nextDouble() < this.consid_rate) {
+				if (random.nextDouble() < this.adjust_rate) {
+					int random_from_memory = random.nextInt(memory.size());
+					double value = memory.get(random_from_memory).getNewX()[i];
+					value = taskProblem.feasible(value + this.range*random_double_in_constraints(-1.0, 1.0), i);
+					ds[i] = value;
+				}
+
+				//double value = memory.get(random_from_memory).getI().getX();
+			}
+			else
+			{
+				//DONE: has to be random number valid for this task
+				ds[i] = taskProblem.feasible(random_double_in_constraints(-5.0, 5.0), i);
+			}
+		}
+		return ds;
+	}
+	
 	@Override
 	public Individual run(Task taskProblem) throws StopCriteriaException {
 		max_iter = taskProblem.getMaxEvaluations();
 		factor = taskProblem.getDimensions();
-		Individual best = taskProblem.getRandomIndividual();
+		
+		random = new Random();
 		List <Individual> memory = initialize_harmony_memory(taskProblem);
-		System.out.println("run");
+		Individual best = new Individual(memory.get(0));
+		/*System.out.println("run");
 		for(Individual mem : memory) {
 			System.out.println(mem);
 		}
-		System.out.println(memory.size());
+		System.out.println(memory.size());*/
+		while (taskProblem.isStopCriteria() == false) {
+			Individual harm = taskProblem.eval(create_harmony(memory, taskProblem));
+			if (taskProblem.isFirstBetter(harm, best)) {
+				best = new Individual(harm);
+			}
+			memory.add(harm);
+			Collections.sort(memory, new SortIndividualByFitness());
+			memory.remove(memory.size()-1);
+			if (debug) {
+				String str = String.format(" > iteration=%d,  fitness=%.4f", taskProblem.getNumberOfEvaluations(), best.getEval());
+				System.out.println(str);
+			}
+		}
 		
 		return best;
 	}
@@ -66,6 +124,7 @@ public class HarmonySearch extends Algorithm {
 	@Override
 	public void resetDefaultsBeforNewRun() {
 		// TODO Auto-generated method stub
+		random = new Random();
 
 	}
 
